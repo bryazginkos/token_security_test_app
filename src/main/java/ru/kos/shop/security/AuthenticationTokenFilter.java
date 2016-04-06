ac65f5302ae5fbd73d519ea78fac56474b32735b
Konstantin <bryazginkos@yahoo.com>
2016-04-07 00:33:29 +0600

    use header

--- src/main/java/ru/kos/shop/security/AuthenticationTokenFilter.java
+++ src/main/java/ru/kos/shop/security/AuthenticationTokenFilter.java
@@ -5,13 +5,15 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * Created by Константин on 05.04.2016.
  */
 public class AuthenticationTokenFilter implements Filter {
+
+    private static final String X_ACCESS_TOKEN = "X_ACCESS_TOKEN";
 
     @Override
     public void init(FilterConfig fc) throws ServletException {
@@ -21,8 +23,11 @@ public class AuthenticationTokenFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain fc) throws IOException, ServletException {
         SecurityContext context = SecurityContextHolder.getContext();
         context.setAuthentication(null);
-        Map<String, String[]> params = req.getParameterMap();
-        String token = !params.isEmpty() && params.containsKey("auth_token") ? params.get("auth_token")[0] : null; //todo header
+
+        String token = null;
+        if (req instanceof HttpServletRequest) {
+            token = ((HttpServletRequest) req).getHeader(X_ACCESS_TOKEN);
+        }
 
         Authentication auth = new TokenAuthentication(token);
         SecurityContextHolder.getContext().setAuthentication(auth);
