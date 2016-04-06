f84c9a61c85c282dedcb2cab5e56223f5071d425
Konstantin <bryazginkos@yahoo.com>
2016-04-07 00:34:22 +0600

    default constructor for json converter

--- src/main/java/ru/kos/shop/domain/Product.java
+++ src/main/java/ru/kos/shop/domain/Product.java
@@ -28,6 +28,9 @@ public class Product implements Serializable {
         this.price = price;
     }
 
+    public Product() {
+    }
+
     public void setId(Integer id) {
         this.id = id;
     }
